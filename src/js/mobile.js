f27ab6d51bf610fce7f6e91f53802a71bf7673d5
Caio <whoiscaio@protonmail.com>
2021-05-13 16:35:20 -0300

    Info Section Stylization and Responsivity Concluded

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('body');
 const menuIcon = document.querySelector('.mobile-menu-icon');
 const menu = document.querySelector('.menu')
+const images = document.getElementsByClassName('change-img');
 
 function toggleanimation() {
   console.log('toggle');
@@ -15,3 +16,22 @@ function toggleanimation() {
     menu.classList.add('animationin');
   }
 }
+
+function verifier() {
+  if(window.innerWidth <= 800) {
+    images[0].src = "src/assets/illustration-editor-mobile.svg";
+    images[1].src = "src/assets/illustration-laptop-mobile.svg";
+  }else {
+    images[0].src = "src/assets/illustration-editor-desktop.svg";
+    images[1].src = "src/assets/illustration-laptop-desktop.svg";
+  }
+
+  if(window.innerWidth <= 1000) {
+    images[1].src = "src/assets/illustration-laptop-mobile.svg";
+  }else {
+    images[1].src = "src/assets/illustration-laptop-desktop.svg";
+  }
+};
+
+verifier();
+window.addEventListener('resize', verifier);
